4ef4c702c72a06f666be4d597f6b6e3315532315
parvezAhmedWEB <parvezahmedprottoy.web@gmail.com>
2024-03-12 00:12:03 +0600

    added global error handler

--- to-do-list-project/app.js
+++ to-do-list-project/app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
+const { notFoundHandler, errorHandler } = require("./src/util/error/error");
 
 // use global middleware
 app.use(require("./src/middlewares/global.middleware"));
+
+// global error handler
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 module.exports = app;
---
+++ to-do-list-project/src/util/error/error.js
@@ -0,0 +1,19 @@
+const notFoundHandler = (req, res, next) => {
+  const err = new Error("Page Not Found!!!");
+  err.status = 404;
+  next(err);
+};
+const errorHandler = (err, req, res, next) => {
+  if (err.status) {
+    return res.status(err.status).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
+  return res.status(500).json({
+    status: 500,
+    message: "Server  Error",
+    error: err.message,
+  });
+};
+module.exports = { notFoundHandler, errorHandler };
