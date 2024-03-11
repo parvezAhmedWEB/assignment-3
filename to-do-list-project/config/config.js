5dbe976090ed28bc51e984b4c528736fe641cbef
parvezAhmedWEB <parvezahmedprottoy.web@gmail.com>
2024-03-12 01:34:27 +0600

    added login user

--- to-do-list-project/config/config.js
+++ to-do-list-project/config/config.js
@@ -5,7 +5,9 @@ const dev = {
   db: {
     url: "mongodb+srv://practices:parvez4040@cluster0.gxg0obw.mongodb.net/to-do-list",
   },
-  token: {},
+  token: {
+    key: "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTcxMDE4NDg4OSwiaWF0IjoxNzEwMTg0ODg5fQ.T865PIexIdtDhXnU6Su7o4BCu7n_bMsCUfIaPBkaxWM",
+  },
 };
 
 module.exports = dev;
--- to-do-list-project/src/controllers/users.controller.js
+++ to-do-list-project/src/controllers/users.controller.js
@@ -1,4 +1,6 @@
 const UserModel = require("../models/users.model");
+const jwt = require("jsonwebtoken");
+const key = require("../../config/config").token.key;
 
 const registerUser = async (req, res) => {
   try {
@@ -27,6 +29,44 @@ const registerUser = async (req, res) => {
     return res.status(500).json({
       success: false,
       message: "User is not created.",
+      error: error.message,
+    });
+  }
+};
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Input filed is not valid.",
+      });
+    }
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "User not found.",
+      });
+    }
+    if (!(user.password === password)) {
+      return res.status(400).json({
+        success: false,
+        message: "Password does not match.",
+      });
+    }
+    const payload = { user };
+    const token = jwt.sign(payload, key, { expiresIn: "7d" });
+    return res.status(200).json({
+      success: true,
+      message: "User login successfully.",
+      payload,
+      token,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "User is not login.",
       error: error.message,
     });
   }
